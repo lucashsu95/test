f90cc461d3b2bdeafd563ce7c69a7e93ceb09104
lucas <lucashsu95@gmial.com>
2023-04-02 21:45:40 +0800

    202304020945

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,27 +1,26 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink, RouterView } from "vue-router";
 import { ref } from "vue";
-import { computed } from '@vue/reactivity';
+import { computed } from "@vue/reactivity";
 
 const countList = ref([
   {
-     id:1,
-     value:"this is a apple",
-     check:false,
-  }
-  ,{
-     id:2,
-     value:"i am John",
-     check:false,
-    }
-  ]);
-
+    id: 1,
+    value: "this is a apple",
+    check: false,
+  },
+  {
+    id: 2,
+    value: "i am John",
+    check: false,
+  },
+]);
 
 const page = ref(0);
 
 const pages = ref({
-  'TodoList':0,
-  '台幣、日幣、美幣換算':1,
+  TodoList: 0,
+  "台幣、日幣、美幣換算": 1,
 });
 
 // ----------TodoList----------
@@ -30,26 +29,23 @@ const count = ref(3);
 const text = ref("");
 
 const addCount = () => {
-
   count.value++;
 
   const id = count.value;
   const value = text.value;
 
   const item = {
-     id: id,
-     value: value
-  }
+    id: id,
+    value: value,
+  };
   countList.value.push(item);
   text.value = "";
-
 };
 
 const closeItem = (e) => {
   const id = e.target.dataset.id;
-  countList.value = countList.value.filter(e => e.id != id);
+  countList.value = countList.value.filter((e) => e.id != id);
 };
-
 
 // ----------台幣、日幣、美幣換算----------
 
@@ -61,47 +57,47 @@ const usd = ref(0);
 </script>
 
 <template>
-
-
-  <button v-for="key,value in pages" :class='{active:page === key}' @click="page = key">{{ value }}</button>
+  <button v-for="(key, value) in pages" :class="{ active: page === key }" @click="page = key">
+    {{ value }}
+  </button>
 
   <section v-show="page === 0">
     <h1>TodoList</h1>
-    <hr>
+    <hr />
     <div>
-      <input type="text" name="add" v-model="text" placeholder="請輸入待辦事項"/>
+      <input type="text" name="add" v-model="text" placeholder="請輸入待辦事項" />
       <button @click="addCount">Add</button>
     </div>
     <div class="container">
       <div v-for="key in countList" :key="key">
-        <input type="checkbox" name="checkbox" v-model="key.check">
-        <h2 class='content' :class="{active:key.check}" v-text="key.value"></h2>
+        <input type="checkbox" name="checkbox" v-model="key.check" />
+        <h2 class="content" :class="{ active: key.check }" v-text="key.value"></h2>
         <button :data-id="key.id" @click="closeItem">x</button>
       </div>
     </div>
-</section>
+  </section>
 
-<section v-show="page === 1">
-  <h1>台幣、日幣、美幣換算</h1>
+  <section v-show="page === 1">
+    <h1>台幣、日幣、美幣換算</h1>
 
-  <hr>
+    <hr />
 
-  <div>
-    <p><label for="twd">台幣：<input type="text" name="twd" id="twd" v-model="twd"></label>元</p>
-    <p><label for="jpy">日幣：<input type="text" name="jpy" id="jpy" v-model="jpy"></label>元</p>
-    <p><label for="usd">美幣：<input type="text" name="usd" id="usd" v-model="usd"></label>元</p>
-  </div>
+    <div>
+      <p>
+        <label for="twd">台幣：<input type="text" name="twd" id="twd" v-model="twd" /></label>元
+      </p>
+      <p>
+        <label for="jpy">日幣：<input type="text" name="jpy" id="jpy" v-model="jpy" /></label>元
+      </p>
+      <p>
+        <label for="usd">美幣：<input type="text" name="usd" id="usd" v-model="usd" /></label>元
+      </p>
+    </div>
 
-  <hr>
+    <hr />
 
-  <p>
-    1 日幣 = 0.278 台幣
-  </p>
-  
-</section>
-
-
-
+    <p>1 日幣 = 0.278 台幣</p>
+  </section>
 </template>
 
 <style scoped>
@@ -114,8 +110,7 @@ const usd = ref(0);
   align-items: center;
 }
 
-.content.active{
-  text-decoration:line-through;
+.content.active {
+  text-decoration: line-through;
 }
-
 </style>
