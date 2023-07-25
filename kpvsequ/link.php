ccd690192664711b455b677f85dd0d7b42713beb
lucashsu95 <lucashsu95@gmail.com>
2023-07-25 22:46:45 +0800

    2023-07-25

--- kpvsequ/link.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start();
-date_default_timezone_set('Asia/Taipei');
-$date = date('Y-m-d');
-$db = new PDO('mysql:host=127.0.0.1;dbname=kpvs','root','');    
--- kpvsequ/login/logout.php
+++
@@ -1,4 +0,0 @@
-<?php
-include '../link.php';
-unset($_SESSION['admin']);
-header('location:.././');
