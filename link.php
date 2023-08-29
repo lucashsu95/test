1dde94725947af54bdb1ca452de236d6351e2b67
lucashsu95 <lucashsu95@gmail.com>
2023-08-29 22:53:06 +0800

    0829_22:53

--- link.php
+++
@@ -1,9 +0,0 @@
-<style>
-    a {
-        text-decoration: none;
-        color: #39f;
-    }
-</style>
-<?php
-session_start();
-$db = new PDO('mysql:host=localhost;dbname=web01', 'admin', '1234');
--- users/del.php
+++
@@ -1,5 +0,0 @@
-<?php
-$id = $_GET['id'];
-include '../link.php';
-$sql = $db->query("DELETE FROM `users` WHERE id=$id");
-header('location: users.php');
