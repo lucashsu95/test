52d8f6f25b8fd567b2dd4ca54c1943844d2d60ae
lucashsu95 <lucashsu95gmail.com>
2024-04-30 16:04:36 +0800

    Update file

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hi</h1>
+    <?php
+    echo 'hi';
+    ?>
+    <?php
+    for ($i=0; $i < 8; $i++) { 
+        echo '<li>';
+    }
+    ?>
+</body>
+</html>
